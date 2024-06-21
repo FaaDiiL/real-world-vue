48b293264b6bd408d4dbb9205e7a07b2cdcf873f
fadil <your-username@users.noreply.github.com>
2024-06-21 14:00:40 +0200

    create vue-app add component and style the app

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+        <nav>
+          <RouterLink to="/">Home</RouterLink> |
+          <RouterLink to="/about">About</RouterLink>
+        </nav>
+      </div>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+$color_1: #2c3e50;
+$color_2: #42b983;
+$font-family_1: Avenir, Helvetica, Arial, sans-serif;
+
+#layout {
+  font-family: $font-family_1;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $color_1;
+}
+nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: $color_1;
+  }
+  a.router-link-exact-active {
+    color: $color_2;
+  }
+}
+h2 {
+  font-size: 20px;
+}
+</style>
